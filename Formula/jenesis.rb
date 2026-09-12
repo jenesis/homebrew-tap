94850eadea03c31108e5667b5c7bb92f358ef082
jreleaserbot <jreleaser@kordamp.org>
2026-09-12 12:56:00 +0000

    jenesis v0.13.0

--- Formula/jenesis.rb
+++ Formula/jenesis.rb
@@ -3,20 +3,30 @@
 class Jenesis < Formula
   desc "A Java-native build tool."
   homepage "https://github.com/jenesis/jenesis"
-  url "https://github.com/jenesis/jenesis/releases/download/v0.12.0/jenesis-0.12.0.zip"
-  version "0.12.0"
-  sha256 "33249b9f373e2d4b4042993e625bb49d9db9bf407485cfb741ffca4230cca904"
+  url "https://github.com/jenesis/jenesis/releases/download/v0.13.0/jenesis-0.13.0.zip"
+  version "0.13.0"
+  sha256 "d9d96e8ae7fc5ad87e584b6cf618e54150dcec80272698605042eecb06751ade"
   license "Apache-2.0"
 
   depends_on "openjdk@25"
 
   def install
     libexec.install Dir["*"]
-    bin.install_symlink "#{libexec}/bin/jenesis" => "jenesis"
+    Dir["#{libexec}/bin/*"].each do |command|
+      name = File.basename(command)
+      next if name.end_with?(".bat")
+      if name == "jenesis-switch"
+        # sourced by the calling shell, so it is linked rather than wrapped in an exec
+        bin.install_symlink command => name
+      else
+        bin.write_exec_script command
+      end
+    end
   end
 
   test do
-    output = shell_output("#{bin}/jenesis --version")
-    assert_match "0.12.0", output
+    system bin/"jenesis-init"
+    assert_predicate testpath/"build/jenesis/jenesis.version", :exist?
+    assert_match version.to_s, shell_output("#{bin}/jenesis-version")
   end
 end
