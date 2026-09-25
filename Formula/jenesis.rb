85ee1124af1ffde3e91a2fc7f460ae1529855e29
jreleaserbot <jreleaser@kordamp.org>
2026-09-25 13:01:22 +0000

    jenesis v0.15.1

--- Formula/jenesis.rb
+++ Formula/jenesis.rb
@@ -3,9 +3,9 @@
 class Jenesis < Formula
   desc "A Java-native build tool."
   homepage "https://github.com/jenesis/jenesis"
-  url "https://github.com/jenesis/jenesis/releases/download/v0.15.0/jenesis-0.15.0.zip"
-  version "0.15.0"
-  sha256 "856b58caf1aeec691825830e65f1227f14e99eea9afeb64b09e25803e858e8c9"
+  url "https://github.com/jenesis/jenesis/releases/download/v0.15.1/jenesis-0.15.1.zip"
+  version "0.15.1"
+  sha256 "62939d9941495969b161d6cc1f7c5b11ee20499fbbeefc212273d317730d3d2f"
   license "Apache-2.0"
 
   depends_on "openjdk@25"
