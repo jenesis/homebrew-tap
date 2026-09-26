5de75aae9590149ce85de5b476085df562a3b374
jreleaserbot <jreleaser@kordamp.org>
2026-09-26 01:03:39 +0000

    jpx v0.15.2

--- Formula/jpx.rb
+++ Formula/jpx.rb
@@ -3,9 +3,9 @@
 class Jpx < Formula
   desc "Runs a published module or Maven artifact with one command"
   homepage "https://github.com/jenesis/jenesis"
-  url "https://github.com/jenesis/jenesis/releases/download/v0.15.1/jpx-0.15.1.zip"
-  version "0.15.1"
-  sha256 "eda219a9c29f9cd87d1c282a050450c0544e41ecbf67125725c332a93755bb04"
+  url "https://github.com/jenesis/jenesis/releases/download/v0.15.2/jpx-0.15.2.zip"
+  version "0.15.2"
+  sha256 "7bfc185f48e83b5c24a8a8fd92baeae9a13733c2ab5c0ddf83ec506ff1b1f53f"
   license "Apache-2.0"
 
   depends_on "openjdk@25"
